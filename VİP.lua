f18e16d2a634f34ab7e004b4abf7287409f6adf1
HamzaMetin <77755948+HamzaMetin@users.noreply.github.com>
2021-02-02 11:12:53 +0300

    Create VİP.lua

---
+++ VİP.lua
@@ -0,0 +1,400 @@
+endtime = load("return os.time{year=2021, month=02, day=07, hour=23,min=59, sec=59}")()
+if os.time() > endtime then
+ gg.alert("💛Script Süresi Doldu Yeni Scripti Telegramdan Alabilirsin 💙  \n 🗯TG:GhhostHack")
+ os.exit()
+end
+
+gg.alert("HOŞ GELDİN GHOST TEAM İLK SCRİPT💝 ")
+LHPS = gg.prompt({
+":closed_lock_with_key:Parola Lütfen"
+}, {}, {})
+if not LHPS then
+ os.exit()
+end
+if LHPS[1] == "4848" then
+ gg.toast("Şifre Doğru Hoşgeldiniz✅ ")
+else
+gg.alert("YANLIŞ PAROLA❌")
+os.exit()
+end
+
+gg.setVisible(false)
+gg.sleep(200)
+gg.sleep(1000)
+gg.toast("🅶︎")
+gg.sleep(400)
+gg.toast("🅶🅷")
+gg.sleep(400)
+gg.toast("🅶🅷🅾︎")
+gg.sleep(400)
+gg.toast("🅶🅷🅾🆂")
+gg.sleep(400)
+gg.toast("🅶🅷🅾🆂🆃")
+gg.sleep(400)
+gg.toast("🅶🅷🅾🆂🆃 🆃")
+gg.sleep(400)
+gg.toast("🅶🅷🅾🆂🆃 🆃🅴")
+gg.sleep(400)
+gg.toast("🅶🅷🅾🆂🆃 🆃🅴🅰︎")
+gg.sleep(400)
+gg.toast("🅶🅷🅾🆂🆃 🆃🅴🅰🅼 𝗔𝗞𝗧𝗶𝗙💝")
+
+MAIN = 1
+function MAIN()
+ menu1 = gg.choice({
+ 
+     "   \n        [🛡️ANTİBAN🛡️ʸᵉⁿⁱ       \n       [Lobi]\n   ",
+   "     \n        [🔫Silah Hileleri🔫]ʸᵉⁿⁱ       \n       [Oyun]\n   ",
+       "    \n        [🎮Oyun Hileleri🎮]ʸᵉⁿⁱ       \n       [Oyun]\n   ",
+       "      \n      [ 🚕Tüm Araçları Uçurma 🚕ʸᵉⁿⁱ]\n     [Oyun]\n  ",
+  "        \n         [🌆 EĞLENCE MENÜSÜ🌆 ʸᵃᵏⁱⁿᵈᵃ]\n         [OYUN]\n",
+       "ⒺⓍⒾⓉ"
+ }, nil, (os.date("Ghost Team İlk Script V1.0💝 ᵗᵃʳⁱʰ2020/02/02")))
+if menu1 == nil then
+ else
+  if menu1 == 1 then
+   Anti()
+ end
+ if menu1 == 2 then
+   Silah()
+ end
+   if menu1 == 3 then
+   Oyun()
+ end
+     if menu1 == 4 then
+   Arac()
+   end
+   if menu1 == 5 then
+   Eglen()
+   end
+  if menu1 == 6 then
+   Exit()
+   end
+ end
+ GHOST = -1
+end
+
+function  Anti()
+RX = gg.prompt({
+"🛡BYPASS[GL][KR]🛡",
+"💯REPORT CLEAR💯",
+':back:ⒺⓍⒾⓉ:back:',}, {}, {"checkbox","checkbox","checkbox","checkbox","checkbox","checkbox","checkbox","checkbox","checkbox"})
+if RXE == nil then else
+if RXE[1] == true then
+     GL()
+   end
+   if RXE[2] == true then
+     RP()
+   end
+       if RXE[3] == true then
+     MAIN()
+   end
+ end
+ RXEY = 0
+end
+
+function GL()
+
+end
+
+function RP()
+
+end
+
+function Silah()
+ GHOST = gg.multiChoice({
+  "⚜Sekmeme⚜",
+     "🧲Aimbot🧲",
+     "➕ᴄ̴̴ʀ̴̴ᴏ̴̴s̴̴s̴ ̴ʜ̴̴ᴀ̴̴ɪ̴̴ʀ̴➕",
+     "👤MAGIC + HS👤",
+     "🏹Az Sekme🏹",
+     "🌪ᴀ̴̴ɴ̴̴ᴛ̴̴ɪ̴ ̴s̴̴ʜ̴̴ᴀ̴̴ᴋ̴̴ᴇ̴🌪",
+     "ⒺⓍⒾⓉ",
+       }, nil, "\nSilah Menüsü🔫 [@GhostTeam🇹🇷🇹🇷]\n")
+         if GHOST == nil then
+ else
+   if GHOST[1] == true then
+     S()
+   end
+   if GHOST[2] == true then
+     A()
+   end
+   if GHOST[3] == true then
+     C()
+   end
+    if GHOST[4] == true then
+     M()
+   end
+   if GHOST[5] == true then
+     AZ()
+   end
+   if GHOST[6] == true then
+     AL()
+   end
+   if GHOST[7] == true then
+     MAIN()
+   end
+ end
+XGCK = -1
+end
+
+function S()
+gg.clearResults()
+ gg.setRanges(gg.REGION_C_DATA)
+ gg.searchNumber("-309056968;-298841599;-309061065", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+ gg.searchNumber("-298841599", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(5)
+ gg.editAll("0", gg.TYPE_DWORD)
+ gg.clearResults()
+ gg.toast("AZ SEKME AÇILDI✅")
+ gg.alert([[ Sekmeme Açılıyor Tamamen Safe Kod✅]])
+ gg.clearResults()
+gg.clearResults()
+ gg.setRanges(gg.REGION_CODE_APP)
+ gg.searchNumber("-4.7846242e19;-3.8685797e25;-1.3697734e28:41", gg.TYPE_FLOAT)
+ gg.refineNumber("-3.8685797e25", gg.TYPE_FLOAT)
+ gg.getResults(10)
+ gg.editAll("0", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.clearResults()
+ gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+ gg.searchNumber("-6.1549454e27;1.8638966e-20;-1.1144502e28;0::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.refineNumber("-1.1144502e28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(1)
+ gg.editAll("0", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.clearResults()
+ gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+ gg.searchNumber("-2.8111605e28;-3.7444097e28;-1.1144502e28;128.0::", 16, false, 536870912, 0, -1)
+ gg.searchNumber("-1.1144502e28", 16, false, 536870912, 0, -1)
+ gg.getResults(1)
+ gg.editAll("0", 16)
+ gg.clearResults()
+ gg.toast("Anti Shake Aktif✅")
+end
+
+function A()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.searchNumber("2046820354;-336587221:9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2046820354", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("2046820353", gg.TYPE_DWORD)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.searchNumber("2015175168", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(6)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+gg.searchNumber("-476053504;-349478012:189", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("-476053504", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("-476053503;-476053504", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("SAFE AİMBOT ✅")
+end
+
+function C()
+gg.setRanges(gg.REGION_ANONYMOUS)
+ gg.clearResults()
+ gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+ gg.searchNumber("-1.2382424e28;-1.4239333e28;-1.1144502e28;-1.8331474e27;-7.1608877e24::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.refineNumber("-1.1144502e28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(2)
+ gg.editAll("7.5", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.toast("CrossHair Aktif Edildi✅")
+end
+
+function M()
+gg.setRanges(gg.REGION_BAD)
+ gg.searchNumber("-88.66608428955;26", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.searchNumber("26", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(2)
+ gg.editAll("-466", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.searchNumber("-88.73961639404;28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.searchNumber("28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(2)
+ gg.editAll("-568", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.setRanges(gg.REGION_ANONYMOUS)
+ gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.searchNumber("30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(10)
+ gg.editAll("280", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.toast("MAGIC + HS AKTİF✅")
+end
+
+function AZ()
+ gg.clearResults()
+ gg.setRanges(gg.REGION_C_DATA)
+ gg.searchNumber("-309056968;-298841599;-309061065", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+ gg.searchNumber("-298841599", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(5)
+ gg.editAll("0", gg.TYPE_DWORD)
+ gg.clearResults()
+ gg.toast("AZ SEKME AÇILDI✅")
+ gg.clearResults()
+end
+
+function AL()
+gg.clearResults()
+ gg.setRanges(gg.REGION_CODE_APP)
+ gg.searchNumber("-4.7846242e19;-3.8685797e25;-1.3697734e28:41", gg.TYPE_FLOAT)
+ gg.refineNumber("-3.8685797e25", gg.TYPE_FLOAT)
+ gg.getResults(10)
+ gg.editAll("0", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.clearResults()
+ gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+ gg.searchNumber("-6.1549454e27;1.8638966e-20;-1.1144502e28;0::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.refineNumber("-1.1144502e28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(1)
+ gg.editAll("0", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.clearResults()
+ gg.setRanges(gg.REGION_C_DATA | gg.REGION_CODE_APP)
+ gg.searchNumber("-2.8111605e28;-3.7444097e28;-1.1144502e28;128.0::", 16, false, 536870912, 0, -1)
+ gg.searchNumber("-1.1144502e28", 16, false, 536870912, 0, -1)
+ gg.getResults(1)
+ gg.editAll("0", 16)
+ gg.clearResults()
+ gg.toast("Anti Shake Aktif✅")
+end
+
+function Oyun()
+ GHOST = gg.multiChoice({
+   ":tokyo_tower:Anten:tokyo_tower:",
+   "🌫Sis Kaldırma🌫",
+   "🏝Çimen Kaldırma🏝",
+   "💉İleri Uzanan Kol💉",
+   "🔭Yukarı Uzanan Kol🔭",
+   " 🎥İpad Mod 🎥",
+   "ⒺⓍⒾⓉ",
+}, nil, "\nOyun Menüsü🕹 [@GhostTeam🇹🇷🇹🇷]\n")
+ if GHOST == nil then
+ else
+ if GHOST[1] == true then
+     Anten()
+   end
+ if GHOST[2] == true then
+     Sis()
+   end
+ if GHOST[3] == true then
+     Cim()
+   end
+ if GHOST[4] == true then
+     Ileri()
+   end
+ if GHOST[5] == true then
+     Ust()
+   end
+ if GHOST[6] == true then
+     Ipad()
+   end
+ if GHOST[7] == true then
+     MAIN()
+   end
+ end
+ XGCK = -1
+end
+
+function Anten()
+gg.clearResults()
+ gg.setRanges(32)
+ gg.searchNumber("88.50576019287F;87.27782440186F;-100.91194152832F;1F::13", 16, false, 536870912, 0, -1)
+ gg.searchNumber("88.50576019287F;87.27782440186F;1F", 16, false, 536870912, 0, -1)
+ antenvalue0 = gg.getResults(100)
+ gg.editAll("1.96875;1.96875;999;1.96875;1.96875;999", 16)
+ gg.clearResults()
+ gg.toast("ANTEN AKTİF EDİLDİ ✅")
+end
+
+function Sis()
+gg.toast("Sis Kaldırma Eklenecektir✅")
+end
+
+function Cim()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("7499628;1935766087;29541::", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.searchNumber("29541", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResultCount()
+gg.getResults(1)
+gg.editAll("1886999666", gg.TYPE_DWORD)
+gg.toast("Çimen Kaldırma Aktif ✅")
+gg.clearResults()
+end
+
+function Ileri()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.setVisible(false)
+gg.searchNumber("4138667321167981973", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("4138667321167981973", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("4138667321167981973", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(200)
+gg.editAll("4848124999984742400", gg.TYPE_QWORD)
+gg.clearResults()
+ gg.toast("Uzun Kol ✅")
+end
+
+function Ust()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.clearResults()
+gg.searchNumber("1,100,159,584", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("1,122,159,584", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Yukarı Uzayan Kol ✅")
+end
+
+
+function Ipad()
+ gg.setRanges(gg.REGION_ANONYMOUS)
+ gg.clearResults()
+ gg.searchNumber("220;178;15 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.searchNumber("220", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(300)
+ gg.editAll("500", gg.TYPE_FLOAT)
+ gg.clearResults()
+ gg.toast("GÖRÜNÜM AKTİF ✅:")
+end
+
+
+function Arac()
+gg.clearResults()
+ gg.setRanges(gg.REGION_ANONYMOUS)
+ gg.searchNumber("-980", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1, 0)
+ revert = gg.getResults(61, nil, nil, nil, nil, nil, nil, nil, nil)
+ gg.editAll("20000", gg.TYPE_FLOAT)
+ gg.processResume()
+ gg.sleep(500)
+ if revert ~= nil then
+   gg.setValues(revert)
+ end
+ gg.clearResults()
+gg.toast("UÇUYORUZ√")
+end
+
+function Exit()
+print("Hile İle Sorun Olursa Telegrama Bildirin☢️")
+print("💟TG:GhhostHack")
+ gg.skipRestoreState()
+ os.exit()
+ end
+ 
+   while true do
+ if gg.isVisible(true) then
+   gg.setVisible(false)
+ MAIN()
+ end
+ if MAIN == 1 then
+   MAIN()
+ end
+end
